1122a701bd6b2249bde872e362fa80ef0b0db335
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-07-18 10:14:00 -0600

    adding kiln function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 // Imports go first
 import { makePottery } from './PotteryWheel.js'
+import { firePottery } from './Kiln.js'
 
 
 // Make 5 pieces of pottery at the wheel
@@ -12,8 +13,15 @@ let platter = makePottery("platter", 6, 2)
 console.log(mug, bowl, vase, plate, platter)
 
 
-// Fire each piece of pottery in the kiln
 
+// Fire each piece of pottery in the kiln
+const firedMug = firePottery(mug, 1900)
+const firedBowl = firePottery(bowl, 2200)
+const firedVase = firePottery(vase, 2300)
+const firedPlate = firePottery(plate, 2000)
+const firedPlatter = firePottery(platter, 2400)
+
+console.log(firedMug, firedBowl, firedVase, firedPlate, firedPlatter)
 
 // Determine which ones should be sold, and their price
 
